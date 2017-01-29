0e6d838b2a5fd40e5fb507013bcbb22ca440fe63
Zegis <kjgm@poczta.onet.pl>
2017-01-29 22:05:18 +0100

    Little modified generic MVC 5.0 app with Session Example

---
+++ ResidSession/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RedisWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session.Add("Today", DateTime.UtcNow.DayOfWeek);
+
+            if (Session["Something"] == null)
+                ViewBag.SomethingImportant = "Not from session";
+            else
+                ViewBag.SomethingImportant = Session["Something"];
+
+            return View();
+        }
+
+        public ActionResult BasicExample()
+        {
+            if (Session["Something"] == null)
+                ViewBag.SomethingImportant = "Not from session";
+            else
+                ViewBag.SomethingImportant = Session["Something"];
+
+            return View();
+        }
+
+        public ActionResult SessionExample()
+        {
+            ViewBag.Today = Session["Today"];
+
+            if (Session["Something"] == null)
+                ViewBag.SomethingImportant = "Not from session";
+            else
+                ViewBag.SomethingImportant = Session["Something"];
+
+            return View();
+        }
+
+        public ActionResult AbandonExample()
+        {
+            Session.Abandon();
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SaveSession(string NewValue)
+        {
+            Session["Something"] = NewValue;
+
+            return RedirectToAction("SessionExample");
+        }
+    }
+}
