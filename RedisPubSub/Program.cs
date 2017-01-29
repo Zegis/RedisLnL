882f085c40d01d9fc244fabec8956f7aa15bd461
Zegis <kjgm@poczta.onet.pl>
2017-01-30 00:42:40 +0100

    Added simple pub/sub example

---
+++ RedisPubSub/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisPubSub
+{
+    class Program
+    {
+        private static ConnectionMultiplexer _redis;
+        private static Subscriber[] sub;
+
+        static void Main(string[] args)
+        {
+            _redis = ConnectionMultiplexer.Connect("localhost"); // Connect to localhost on default port
+            sub = new Subscriber[2];
+            for (int i = 0; i < 2; ++i)
+            {
+                sub[i] = new Subscriber(_redis.GetSubscriber(), "Subscriber"+i);
+
+            }
+            sub[0].Subscribe("Mars");
+            sub[1].Subscribe("Phobos");
+            sub[0].Subscribe("Cosmos");
+            sub[1].Subscribe("Cosmos");
+
+            Console.WriteLine("Started listening to messagef from Mars, Phobos and Cosmos...");
+            while (true)
+            {
+                
+
+                if (Console.ReadKey(true).Key == ConsoleKey.Home)
+                        break;
+            }
+
+            Console.WriteLine("Goodbye galaxy...");
+
+        }
+
+    }
+}
---
+++ RedisPubSub/Subscriber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StackExchange.Redis;
+
+namespace RedisPubSub
+{
+    class Subscriber
+    {
+        ISubscriber _subscriber;
+        string _name;
+
+        public Subscriber(ISubscriber sub, string name)
+        {
+            _subscriber = sub;
+            _name = name;
+        }
+
+        public void Subscribe(string channelName)
+        {
+            _subscriber.Subscribe(channelName, (channel, message) => { Console.WriteLine(this._name + " recieved " + message + " from " + channel); });
+        }
+    }
+}
